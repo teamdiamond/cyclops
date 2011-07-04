5eb12f6d6c7e39d325010d1e36273c24f6fc22c2
Wolfgang Pfaff <w.pfaff@tudelft.nl>
2011-07-04 13:10:10 +0200

    python 2.6 here

--- cyclops_example.bat
+++ cyclops_example.bat
@@ -1,9 +1,9 @@
 :: cyclops_example.bat
 :: runs and example cyclops configuration on windows
 
-:: SET PATH=%CD%\..\3rd_party\gtk\bin;%CD%\..\3rd_party\gtk\lib;%PATH%
-:: SET GTK_BASEPATH=%CD%\..\3rd_party\gtk
+SET PATH=%CD%\..\3rd_party\gtk\bin;%CD%\..\3rd_party\gtk\lib;%PATH%
+SET GTK_BASEPATH=%CD%\..\3rd_party\gtk
 SET PATH=C:\Qt\2010.02.1\qt\bin;C:\Qt\2010.02.1\bin;C:\Qt\2010.02.1\mingw\bin;%PATH%
 
-c:\python27\python.exe source/start_cyclops.py scan_example.py
+c:\python26\python.exe source/start_cyclops.py scan_example.py
 :exit
