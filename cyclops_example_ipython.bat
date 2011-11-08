7d4fa7913af87293bd019551f11e017f6b3180b2
Wolfgang Pfaff <w.pfaff@tudelft.nl>
2011-11-08 18:47:52 +0100

    included correct launcher for python27 and ipython 0.11

--- cyclops_example_ipython.bat
+++ cyclops_example_ipython.bat
@@ -1,12 +1,15 @@
 :: cyclops_example.bat
 :: runs and example cyclops configuration on windows
 
-SET PATH=%CD%\..\3rd_party\gtk\bin;%CD%\..\3rd_party\gtk\lib;%PATH%
-SET GTK_BASEPATH=%CD%\..\3rd_party\gtk
-SET PATH=%CD%\..\3rd_party\Console2\;%PATH%
-SET PATH=C:\Qt\2010.02.1\qt\bin;C:\Qt\2010.02.1\bin;C:\Qt\2010.02.1\mingw\bin;%PATH%
 
-start Console -w "Cyclops" -r "/k c:\python26\python c:\python26\scripts\ipython.py -gthread -p sh source/start_cyclops.py scan_example.py"
+SET PATH=%CD%\..\qtlab\3rd_party\Console2\;%PATH%
+:: SET PATH=C:\Qt\2010.02.1\qt\bin;C:\Qt\2010.02.1\bin;C:\Qt\2010.02.1\mingw\bin;%PATH%
+
+:: use this for python 2.6/ipython 0.10
+:: start Console -w "Cyclops" -r "/k c:\python27\python c:\python27\scripts\ipython-script.py --q4thread -p sh source/start_cyclops.py scan_example.py"
+
+:: use this for python27 with ipython 0.11
+start Console -w "Cyclops" -r "/k c:\python27\python c:\python27\scripts\ipython-script.py --gui=qt -i source/start_cyclops.py scan_example.py"
 
 :: c:\python27\python.exe source/start_cyclops.py scan_example.py
 exit
