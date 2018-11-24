ff98484b0a7f003b1ff6dae8847a3d41de11d011
Team diamond lt2 <fdg@fdg.com>
2018-11-24 14:38:57 +0100

    Lt2_old to create a remote branch to retrack old software

--- cyclops_LT2_Scanning_ipython.bat
+++ cyclops_LT2_Scanning_ipython.bat
@@ -2,7 +2,9 @@
 :: runs and example cyclops configuration on windows
 
 
-SET PATH=%CD%\..\qtlab\3rd_party\Console2\;%PATH%
+:: SET PATH=%CD%\..\qtlab\3rd_party\Console2\;%PATH%
+SET PATH=C:\Console2\;%PATH%
+
 SET QT_API=pyqt
 :: SET PATH=C:\Qt\2010.02.1\qt\bin;C:\Qt\2010.02.1\bin;C:\Qt\2010.02.1\mingw\bin;%PATH%
 
@@ -10,7 +12,8 @@ SET QT_API=pyqt
 :: start Console -w "Cyclops" -r "/k c:\python27\python c:\python27\scripts\ipython-script.py --q4thread -p sh source/start_cyclops.py scan_example.py"
 
 
-SET PYTHON_PATH=C:\DOCUME~1\localadmin\LOCALS~1\APPLIC~1\Enthought\Canopy32\User\Scripts
+::SET PYTHON_PATH=C:\DOCUME~1\localadmin\LOCALS~1\APPLIC~1\Enthought\Canopy32\User\Scripts
+SET PYTHON_PATH=C:\Canopy\User\scripts
 :: use this for python27 with ipython 0.11
 start Console -w "Cyclops::LT2 Scanning" -r "/k %PYTHON_PATH%\python.exe %PYTHON_PATH%\ipython-script.py -i source/start_cyclops.py lt2_scanning.py"
 
